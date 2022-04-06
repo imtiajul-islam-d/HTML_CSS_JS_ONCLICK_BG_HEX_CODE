b863654a09fa8b094cc95f4ca3a5f27338cd63d2
imtiajul37 <imtiajul37@gmail.com>
2022-04-06 21:44:13 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,30 @@
+    const button = document.querySelector('#btn')
+    const body = document.querySelector('body')
+    const hexValues = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F']
+    const value = document.querySelector('#hex-value')
+
+    button.addEventListener('click', changeHex)
+
+    function changeHex(){
+        let hex = '#'
+
+        for (let i = 0; i < 6; i++){
+            const index = Math.floor(Math.random()*hexValues.length)
+            hex += hexValues[index]
+            console.log(hex)
+        }
+        value.textContent = hex
+        body.style.backgroundColor = hex
+        
+    }
+    
+    // button.addEventListener('click' , changeHex);
+    // function changeHex(){
+        
+    //     for(let i = 0; i < 6; i++){
+    //         const index = Math.floor(Math.random() * hexValues.length);
+    //         hex += hexValues[index]
+    //     }
+    //     body.style.backgroundColor = hex;
+    //     value.textContent = hex;
+    // }
